e7753de836fd655f590daa50cc8979e9bd157c65
Fezalkhalifa <fezalkhalifa1995@gmail.com>
2022-01-03 14:25:59 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <task-list
+      :tasks="tasks"
+      @clearCompleted="clearCompletedTask()"
+      @clearAll="clearAllTask()"
+    ></task-list>
+  </div>
+</template>
+
+
+<script>
+import TaskList from "./components/TaskList.vue";
+export default {
+  name: "App",
+  components: {
+    TaskList,
+  },
+  data: function () {
+    return {
+      tasks: [
+        {
+          title: "Make todo list",
+          completed: true,
+        },
+        {
+          title: "Go skydiving",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    clearCompletedTask() {
+      this.tasks = this.tasks.filter(this.inProgress);
+      console.log(this.tasks);
+    },
+    inProgress(task) {
+      return !this.isCompleted(task);
+    },
+    clearAllTask() {
+      this.tasks = [];
+    },
+    isCompleted(task) {
+      return task.completed;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,133 @@
+<template id="task-list">
+  <section class="tasks">
+    <h1>
+      Tasks
+      <transition name="fade">
+        <small v-if="incomplete">({{ incomplete }})</small>
+      </transition>
+    </h1>
+    <div class="tasks__new input-group pull-center">
+      <input
+        type="text"
+        class="input-group-field"
+        v-model="newTask"
+        @keyup.enter="addTask"
+        placeholder="New task"
+      />
+      <span class="input-group-button">
+        <button @click="addTask" class="btn btn-primary">Add</button>
+      </span>
+    </div>
+    <div style="margin-top: 5px" class="tasks__clear btn-group pull-right">
+      <button
+        style="margin-right: 16px"
+        class="btn btn-warning btn-sm"
+        @click="$emit('clearCompleted')"
+      >
+        <i v-if="true" key="clearC" class="far fa-check-square"></i> Clear
+        Completed
+      </button>
+      <div class="clearfix"></div>
+      <button
+        style="margin-right: 16px"
+        class="btn-danger btn-sm"
+        @click="$emit('clearAll')"
+      >
+        <i key="clearA" class="fa fa-trash"></i> Clear All
+      </button>
+    </div>
+    <div class="clear-fix"></div>
+    <transition-group
+      style="margin-top: 5px"
+      name="fade"
+      tag="ul"
+      class="tasks__list no-bullet"
+    >
+      <task-item
+        v-for="(task, index) in tasks"
+        @remove="removeTask(index)"
+        @complete="completeTask(task)"
+        :task="task"
+        :key="index"
+      ></task-item>
+    </transition-group>
+  </section>
+</template>
+
+<script>
+import TaskItem from "./TaskItem.vue";
+
+export default {
+  name: "TaskList",
+  components: {
+    TaskItem,
+  },
+  props: {
+    tasks: { default: [] },
+  },
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  computed: {
+    incomplete() {
+      return this.tasks.filter(this.inProgress).length;
+    },
+  },
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.tasks.push({
+          title: this.newTask,
+          completed: false,
+        });
+        this.newTask = "";
+      }
+    },
+    completeTask(task) {
+      task.completed = !task.completed;
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    inProgress(task) {
+      return !this.isCompleted(task);
+    },
+    isCompleted(task) {
+      return task.completed;
+    },
+  },
+};
+</script>
+<style>
+.tasks {
+  width: 100%;
+  max-width: 45rem;
+  padding: 1em;
+  margin: 1em auto;
+  overflow: auto;
+  background-color: white;
+  box-shadow: 0px 0.25rem 1rem rgba(black, 0.25);
+}
+.tasks__list {
+  clear: both;
+}
+
+body {
+  background-color: #abc;
+}
+*,
+h1,
+button {
+  font-family: "Nunito", sans-serif;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
